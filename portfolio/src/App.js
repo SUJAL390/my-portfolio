d9132cb9ab4d0857d5d0d5028a61a94ac286357f
SUJAL390 <85659920+SUJAL390@users.noreply.github.com>
2023-03-03 15:29:25 +0545

    Add files via upload

---
+++ portfolio/src/App.js
@@ -0,0 +1,27 @@
+import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
+import "./App.css";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import { useContext } from "react";
+import { themeContext } from "./Context";
+function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <Navbar />
+      <Intro />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
